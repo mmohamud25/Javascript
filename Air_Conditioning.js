ac6f5987720af0938e694d0b000eafd7344e7716
mmohamud25 <149164234+mmohamud25@users.noreply.github.com>
2023-11-28 21:39:22 -0500

    Create Air_Conditioning.js
    Air Condition Program

---
+++ Air_Conditioning.js
@@ -0,0 +1,9 @@
+//air conditioning 
+//const isWeekday = true;
+const isWeekday = false;
+//var airConditioning = true;
+var airConditioning = !true;
+console.log("Weekday");
+console.log(isWeekday);
+console.log('Air conditioning');
+console.log(airConditioning);
