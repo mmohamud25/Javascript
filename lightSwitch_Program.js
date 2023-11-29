edcee9069d71118a031c8fc507683202d4d8f870
Mohamed Mohamud <149164234+mmohamud25@users.noreply.github.com>
2023-11-29 10:45:04 -0500

    Create lightSwitch_Program.js
    Light Switch Program

---
+++ lightSwitch_Program.js
@@ -0,0 +1,19 @@
+//Light Switch Program
+
+var isDay = !true;
+var lightsOn = !false;
+console.log('Daytime?');
+console.log(isDay);
+
+console.log("Lights on?");
+console.log(lightsOn);
+
+
+//Use the negation operator to change the variables inside the display statements.
+//Try combining the display statements into only two statements in total.
+
+var isDay = !true;
+var lightsOn = !false;
+
+console.log('Daytime?', !isDay);
+console.log("Lights on?", !lightsOn);
