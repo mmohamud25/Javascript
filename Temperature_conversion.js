6e1dc32550f5a6ce1d254f3f2a9d8f45a26833a3
mmohamud25 <149164234+mmohamud25@users.noreply.github.com>
2023-11-28 21:32:50 -0500

    Create Temperature_conversion.js
    This code converts temperature

---
+++ Temperature_conversion.js
@@ -0,0 +1,12 @@
+// Temperature conversions
+var celsius = 5;
+var fahrenheit = 41;
+var celsiusToFahrenheit = (celsius * 9 / 5) + 32;
+var fahrenheitToCelsius = (fahrenheit - 32) * 5 / 9;
+console.log("Temperature conversions");
+console.log('-----------------------');
+console.log(fahrenheit + " degrees Fahrenheit are:");
+console.log(fahrenheitToCelsius + " degrees Celsius");
+console.log('----');
+console.log(celsius + " degrees celsius are:");
+console.log(celsiusToFahrenheit + " degrees fahrenheit");
